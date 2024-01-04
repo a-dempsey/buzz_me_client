36b7053a4b7a42f606fae516e59c0f489dce047e
amy <amydempsey0@gmail.com>
2024-01-04 19:10:30 +0000

    layout for nearest & saved routes screens

--- lib/screens/nearest_routes.dart
+++ lib/screens/nearest_routes.dart
@@ -6,7 +6,26 @@ class NearestRoutes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 70.0),
+            child: Column(
+            children: [
+              const SizedBox(height: 35),
+              // heading
+              Text(
+                'Routes nearest to you',
+                style: TextStyle(
+                  color: Colors.grey[800],
+                  fontSize: 24,
+                  fontFamily: 'Roboto-Medium',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/saved_routes.dart
+++ lib/screens/saved_routes.dart
@@ -6,7 +6,26 @@ class SavedRoutes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 70.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 35),
+              // heading
+              Text(
+                'Your saved routes',
+                style: TextStyle(
+                  color: Colors.grey[800],
+                  fontSize: 24,
+                  fontFamily: 'Roboto-Medium',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
