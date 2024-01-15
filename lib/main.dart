42171f4df8b1bb70d0f99afeaafe9a97bce5f8b2
amy <amydempsey0@gmail.com>
2024-01-15 18:24:35 +0000

    firebase main

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
 import 'package:buzz_me/screens/login_screen.dart';
 import 'package:buzz_me/screens/nav_screen.dart';
 import 'package:buzz_me/screens/register_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
-void main() {
-=======
 import 'firebase_options.dart';
 
 void main()  {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp(
-    name: 'buzz-me',
+    name: 'buzz-me-client',
     options: DefaultFirebaseOptions.currentPlatform,
   );
->>>>>>> 20dd74a (firebase for project)
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -25,8 +22,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-    //  home: LoginScreen(),
-     // home: RegisterScreen(),
+      //  home: LoginScreen(),
+      // home: RegisterScreen(),
       home: NavScreen(),
     );
   }
