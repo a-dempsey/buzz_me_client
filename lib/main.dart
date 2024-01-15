8c751b5960ede84f70c2b8ffdd29af709e69eea2
amy <amydempsey0@gmail.com>
2024-01-15 17:25:37 +0000

    firebase for project

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,18 @@ import 'package:buzz_me/screens/nav_screen.dart';
 import 'package:buzz_me/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 
+<<<<<<< HEAD
 void main() {
+=======
+import 'firebase_options.dart';
+
+void main()  {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    name: 'buzz-me',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+>>>>>>> 20dd74a (firebase for project)
   runApp(const MyApp());
 }
 
