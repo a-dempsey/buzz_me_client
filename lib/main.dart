74db1801c871848ebd7c8c436e89bc7b8b5f8ac0
amy <amydempsey0@gmail.com>
2024-01-15 18:16:39 +0000

    firebase for main

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
- 
+
 void main()  {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp(
