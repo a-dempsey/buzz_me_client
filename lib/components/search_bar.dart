11e08045ae1eae1688615eab2e7d7ef26c961c0a
amy <amydempsey0@gmail.com>
2024-02-08 14:42:19 +0000

    search bar functionality implemented

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -1,10 +1,7 @@
-import 'dart:ffi';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class LocationSearchBar extends StatefulWidget {
-  const LocationSearchBar({super.key});
+  const LocationSearchBar({Key? key}) : super(key: key);
 
   @override
   State<LocationSearchBar> createState() => _LocationSearchBarState();
@@ -12,57 +9,121 @@ class LocationSearchBar extends StatefulWidget {
 
 class _LocationSearchBarState extends State<LocationSearchBar> {
   static bool hasText = false;
-  static String newText = "";
   TextEditingController locationTextController = TextEditingController();
+  OverlayEntry? locationContainer;
+  final List<String> locations = [
+    'Pouladuff Road',
+    'Dennehy\'s Cross',
+    'Black Ash',
+    'Brookfield'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.only(right: 25),
-          child: SearchAnchor(
-              builder: (BuildContext context, SearchController controller) {
-                return SearchBar(
-                  elevation: const MaterialStatePropertyAll<double>(1.0),
-                  hintText: "Search locations",
-                  hintStyle: MaterialStateProperty.all<TextStyle>(
-                    const TextStyle(
-                      fontFamily: 'Roboto-Medium',
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400
-                    ),
-                  ),
-                  side: MaterialStateProperty.all(BorderSide(color: Colors.pink[100]!.withOpacity(0.7), width: 1)),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    )
-                  ),
-                  controller: locationTextController,
-                  leading: const Icon(Icons.search),
-                  trailing: hasText ? [IconButton(icon: const Icon(Icons.close), onPressed: () { setState(() {hasText = false; locationTextController.clear();});})] : null,
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.pink[100]!),
-                  padding: const MaterialStatePropertyAll<EdgeInsets>(
-                      EdgeInsets.symmetric(horizontal: 16.0)),
-                  onTap: () {
-                   // TODO define search behaviour
-
-                  },
-                  onChanged: (newText) {
+      body: Padding(
+        padding: const EdgeInsets.only(right: 25),
+        child: SearchAnchor(
+          builder: (BuildContext context, SearchController controller) {
+            return SearchBar(
+              elevation: const MaterialStatePropertyAll<double>(1.0),
+              hintText: "Search locations",
+              hintStyle: MaterialStateProperty.all<TextStyle>(
+                const TextStyle(
+                  fontFamily: 'Roboto-Medium',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              side: MaterialStateProperty.all(
+                BorderSide(
+                  color: Colors.pink[100]!.withOpacity(0.7),
+                  width: 1,
+                ),
+              ),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+              controller: locationTextController,
+              leading: const Icon(Icons.search),
+              trailing: hasText ? [
+                IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: () {
                     setState(() {
-                      hasText = newText.isNotEmpty;
+                      hasText = false;
+                      locationTextController.clear();
                     });
                   },
-                );
-              }, suggestionsBuilder:
-              (BuildContext context, SearchController controller) {
+                ),
+              ] : null,
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.pink[50]!),
+              padding: const MaterialStatePropertyAll<EdgeInsets>(
+                EdgeInsets.symmetric(horizontal: 16.0),
+              ),
+              onTap: () {},
+              onChanged: (locationText) {
+                setState(() {
+                  hasText = locationText.isNotEmpty;
+                  List<String> results = [];
+                  if (locationText.isNotEmpty) {
+                    for (var stop in locations) {
+                      if (stop.toLowerCase().contains(locationText.toLowerCase())) {
+                        results.add(stop);
+                      }
+                    }
+                  }
+                  if (results.isNotEmpty) {
+                    locationContainer?.remove();
+                    locationContainer = OverlayEntry(
+                      builder: (BuildContext overlayContext) => Positioned(
+                        top: MediaQuery.of(context).size.height * 0.22,
+                        left: 20.0,
+                        right: 20.0,
+                        child: Card(
+                          elevation: 0,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 40),
+                            child: Column(
+                              children: [
+                                for (var stop in results)
+                                  ListTile(
+                                    title: Text(stop),
+                                    onTap: () {
+                                      locationTextController.text = stop.toString();
+                                      locationContainer?.remove();
+                                      locationContainer = null;
+                                    },
+                                  ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                    WidgetsBinding.instance.addPostFrameCallback(
+                      (_) => Overlay.of(context).insert(locationContainer!)
+                    );
+                    } else {
+                      locationContainer?.remove();
+                      locationContainer = null;
+                    }
+                });
+              },
+            );
+          },
+          suggestionsBuilder: (BuildContext context, SearchController controller) {
             return List<ListTile>.generate(5, (_) {
               return ListTile(
                 onTap: () {
                 },
               );
             });
-          }),
+          },
         ),
+      ),
     );
   }
 }
--- lib/screens/timetable_screen.dart
+++ lib/screens/timetable_screen.dart
@@ -1,3 +1,4 @@
+import 'package:buzz_me/components/search_bar.dart';
 import 'package:flutter/material.dart';
 
 class TimetableScreen extends StatelessWidget {
@@ -5,11 +6,53 @@ class TimetableScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:[
+                    const Text(
+                    'Access route information',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Roboto-Medium',
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 2.5),
+                  Text(
+                    'Search by stop name',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontFamily: 'Roboto-Medium',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  const SizedBox(height: 13.5),
+                  const SizedBox(
+                    height: 50,
+                    child: LocationSearchBar()
+                  ),
+
+                  // SizedBox(height: 15),
+                  // FloatingActionButton(
+                  //   //child: Icon(Icons.person),
+                  //   backgroundColor: Color.fromRGBO(0, 131, 143, 1),
+                  //   foregroundColor: Colors.white,
+                  //   onPressed: null,
+
+    //
+    ],),
+
+              ),
           ],
         ),
       ),
