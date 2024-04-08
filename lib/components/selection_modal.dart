9203358d86e3f39cecc5f52599c2144fe8de31f9
amy <amydempsey0@gmail.com>
2024-04-08 08:12:30 +0100

    changed time selection modal to input

--- lib/components/selection_modal.dart
+++ lib/components/selection_modal.dart
@@ -5,6 +5,7 @@ import 'package:buzz_me/components/upcoming_notification.dart';
 import 'package:buzz_me/screens/timetable_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:time_picker_spinner_pop_up/time_picker_spinner_pop_up.dart';
 import '../notifications/notifications.dart';
 import '../screens/nav_screen.dart';
@@ -21,7 +22,8 @@ class SelectionModal extends StatefulWidget {
 }
 
 class _SelectionModalState extends State<SelectionModal> {
-  static DateTime _mins = DateTime(0);
+ // static DateTime _mins = DateTime(0);
+  int _mins = 0;
   bool showNotification = false;
   bool delivered = false;
   bool check = false;
@@ -66,7 +68,7 @@ class _SelectionModalState extends State<SelectionModal> {
       final localNotificationService = LocalNotificationService();
       await localNotificationService.showLocalNotification(
         'Buzz Me',
-        'Your bus is due to arrive in ${_mins.minute} minutes',
+        'Your bus is due to arrive in ${_mins} minutes',
       );
       _notificationTimer?.cancel();
       print(UpcomingNotification.index);
@@ -147,9 +149,7 @@ class _SelectionModalState extends State<SelectionModal> {
       setState(() {
         UpcomingNotification.time.removeAt(index);
       });
-
     }
-
   }
 
 
@@ -195,15 +195,28 @@ class _SelectionModalState extends State<SelectionModal> {
                   ),
                 ),
               ),
-              TimePickerSpinnerPopUp(
-                mode: CupertinoDatePickerMode.time,
-                initTime: DateTime.parse("0000-00-00 00:00:00"),
-                maxTime: DateTime(0).add(const Duration(hours: 0, minutes: 29)),
-                timeFormat: 'H:mm',
-                onChange: (DateTime value) {
-                    _mins = value;
-                },
+              SizedBox(
+                width: 100,
+                child: TextField(
+                    decoration: const InputDecoration(border: InputBorder.none,icon: Icon(Icons.access_time_rounded), hintText: '00'),
+                    keyboardType: TextInputType.number,
+                    onChanged: (String value){
+                      _mins = int.parse(value);
+                    },
+                    inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                ),
               ),
+              // TimePickerSpinnerPopUp(
+              //   mode: CupertinoDatePickerMode.time,
+              //   initTime: DateTime.parse("0000-00-00 00:00:00"),
+              //   maxTime: DateTime(0).add(const Duration(hours: 0, minutes: 29)),
+              //   timeFormat: 'H:mm',
+              //   onChange: (DateTime value) {
+              //       _mins = value;
+              //   },
+              // ),
               Row(
                 children: [
                   ElevatedButton(
@@ -235,12 +248,11 @@ class _SelectionModalState extends State<SelectionModal> {
                   ElevatedButton(
                     onPressed: () async {
                       Navigator.pop(context);
-
                       setState(() {
                         UpcomingNotification.display = true;
                        DateTime routeTime = DateFormat('HH:mm').parse(SelectionModal.time);
                        // DateTime routeTime = DateTime.thursday as DateTime;
-                        final time = routeTime.subtract(Duration(minutes: _mins.minute));
+                        final time = routeTime.subtract(Duration(minutes: _mins));
                         if(time.minute >= 0 && time.minute < 10) {
                           UpcomingNotification.time.add(("${time.hour}:0${time.minute}"));
                         } else if(time.hour >= 0 && time.hour < 10) {
@@ -265,8 +277,8 @@ class _SelectionModalState extends State<SelectionModal> {
                         transitionDuration: Duration.zero,
                         reverseTransitionDuration: Duration.zero,
                       ),);
-                      if(_mins.minute >= 1) {
-                        ConfirmationToast.showConfirmationToast("A notification has been set for ${_mins.minute} minutes before \nthe bus arrives", context);
+                      if(_mins >= 1) {
+                        ConfirmationToast.showConfirmationToast("A notification has been set for ${_mins} minutes before \nthe bus arrives", context);
                       }
                     },
                     style: ButtonStyle(
